3065a47979cb42fb367a9f5f951943616bb62f51
cetessai2501 <jules.lupus@laposte.net>
2019-08-10 20:58:39 +0200

    commit 3

--- cmd/root.go
+++ cmd/root.go
@@ -2,19 +2,24 @@ package cmd
 
 import (
         "fmt"
+        "log"
         "github.com/spf13/cobra"
         "os"
 )
 
 var developer string
 
+var (
+	// VERSION is set during build
+	VERSION = "0.0.1"
+)
+
+
 var rootCmd = &cobra.Command{
         Use:   "my_flags",
         Short: "A brief description of your application",
         Long:  `A longer description.`,
-        Run: func(cmd *cobra.Command, args []string) {
-                fmt.Println("all in delete was called!")
-        },
+       
 }
 
 func Execute() {
@@ -24,14 +29,25 @@ func Execute() {
         }
 }
 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Show heft.io client version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(rootCmd.Use + " " + VERSION)
+	},
+}
+
+
 func init() {
         cobra.OnInitialize(initConfig)
         rootCmd.PersistentFlags().StringVar(&developer, "developer", "Unknown Developer!", "Developer name.")
-        rootCmd.AddCommand(allCmd)
+        rootCmd.AddCommand(versionCmd)
+        
 }
 
 func initConfig() {
         developer, _ := rootCmd.Flags().GetString("developer")
+        log.Printf("eee", developer)       
         if developer != "" {
                 fmt.Println("Developer:", developer)
         }
